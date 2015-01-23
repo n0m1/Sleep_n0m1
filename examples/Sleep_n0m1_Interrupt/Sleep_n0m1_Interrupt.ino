6b6caeed7e60c0d5cc4ee0b761a2b382569c87c1
Noah Shibley <archive.of.receipts@gmail.com>
2015-01-23 11:23:03 +0900

    Fixed comment

--- examples/Sleep_n0m1_Interrupt/Sleep_n0m1_Interrupt.ino
+++ examples/Sleep_n0m1_Interrupt/Sleep_n0m1_Interrupt.ino
@@ -21,7 +21,10 @@ void loop()
   delay(100); //delay to allow serial to fully print before sleep
     
   sleep.pwrDownMode(); //set sleep mode
-  sleep.sleepInterrupt(0,FALLING); //sleep for: sleepTime
+  
+  //Sleep till interrupt pin equals a particular state. 
+  //In this case "falling" change from 1 to 0.
+  sleep.sleepInterrupt(0,FALLING); //(interrupt Number, interrupt State)
  
  
   
