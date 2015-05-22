0621c4cc9525863f168aa0aa9cb6f2816788dcae
Noah Shibley <archive.of.receipts@gmail.com>
2015-05-22 17:16:35 +0900

    Update Comments

--- Sleep_n0m1.h
+++ Sleep_n0m1.h
@@ -56,19 +56,80 @@ Sleep();
    SLEEP_MODE_PWR_DOWN
    */
 
+	//------------------------------------------------------    
+	// Description: sets the Arduino into idle Mode sleep, 
+	// the least power saving, The idle mode stops the MCU
+	// but leaves peripherals and timers running. 
+	//------------------------------------------------------ 
 	void idleMode() { setSleepMode(SLEEP_MODE_IDLE);}
+	
+	//------------------------------------------------------
+	// Description: sets the Arduino into adc Mode sleep, 
+	// This mode makes the MCU enter ADC Noise Reduction mode,
+	// stopping the CPU but allowing the ADC, the external interrupts,
+	// the 2-wire Serial Interface address watch, Timer/Counter2
+	// and the Watchdog to continue operating.
+	//------------------------------------------------------
 	void adcMode() {setSleepMode(SLEEP_MODE_ADC);}
+	
+	//------------------------------------------------------
+	// Description: sets the Arduino into power Save Mode sleep,
+	// The timer crystal will continue to operate in this mode, 
+	// Timer2 still active.
+	//------------------------------------------------------
 	void pwrSaveMode() {setSleepMode(SLEEP_MODE_PWR_SAVE);}
+	
+	//------------------------------------------------------ 
+	// Description: sets the Arduino into extStandby Mode sleep,
+	// This mode is identical to Power-save with the exception
+	// that the Oscillator is kept running for fast wake up
+	//------------------------------------------------------ 
 	void extStandbyMode(){setSleepMode(SLEEP_MODE_EXT_STANDBY);}
+	
+	//------------------------------------------------------
+	// Description: sets the Arduino into standby Mode sleep,
+	// This mode is identical to Power-down with the exception
+	// that the Oscillator is kept running for fast wake up
+	//------------------------------------------------------
 	void standbyMode(){setSleepMode(SLEEP_MODE_STANDBY);}
+	
+	//------------------------------------------------------
+	// Description: sets the Arduino into power Down Mode sleep,
+	// The most power saving, all systems are powered down 
+	// except the watch dog timer and external reset
+	//------------------------------------------------------
 	void pwrDownMode(){setSleepMode(SLEEP_MODE_PWR_DOWN);}
 
-	//WatchDog Sleep Functions: Sleep for a specfic length of time
+	//------------------------------------------------------  
+	// Description: Works like the Arduino delay function, sets the
+	// Arduino into sleep mode for a specified time.
+	// Parameters: (unsigned long) time in ms of the sleep cycle
+	//------------------------------------------------------  
 	void sleepDelay(unsigned long sleepTime);
+	
+	//------------------------------------------------------
+	// Description: Works like the Arduino delay function, sets the
+	// Arduino into sleep mode for a specified time.
+	// Parameters: (unsigned long) time in ms of the sleep cycle
+	//            (boolean) prevents the Arduino from entering sleep
+	//------------------------------------------------------
 	void sleepDelay(unsigned long sleepTime,boolean &abortCycle);
+	
+	//------------------------------------------------------
+	// Description: the WDT needs to be calibrated against timer 0 
+	// periodically to keep the sleep time accurate. Default calibration
+	// occurs every 100 wake/sleep cycles. recalibrate too often will
+	// waste power and too rarely will make the sleep time inaccurate. 
+	// Parameters: (int) set the # of wake/sleep cycles between calibrations
+	//------------------------------------------------------
 	void setCalibrationInterval(int interval){ sleepCycleInterval = interval; }
 	
-	//Interrupt Sleep Function: Sleep till something interrupts sleep
+	//------------------------------------------------------
+	// Description: set the Arduino into sleep mode until an interrupt is
+	// triggered. The interrupts are passed in as parameters
+	// Parameters: (int) interrupt value, 0, 1, etc, see attachinterrupt()
+	//      (int) mode of trigger, LOW,RISING,FALLING,CHANGE
+	//------------------------------------------------------
 	void sleepInterrupt(int interrupt,int mode);
 	
 	
